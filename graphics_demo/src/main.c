3286474faa8f15f9ac9c4b1e9d75971d00d0f5c7
haydencarroll1 <carrollhayden8@gmail.com>
2023-10-28 22:01:59 +0100

    Added different assets for explosion

--- graphics_demo/src/main.c
+++ graphics_demo/src/main.c
@@ -17,10 +17,11 @@ int isInside(uint16_t x1, uint16_t y1, uint16_t w, uint16_t h, uint16_t px, uint
 void enablePullUp(GPIO_TypeDef *Port, uint32_t BitNumber);
 void pinMode(GPIO_TypeDef *Port, uint32_t BitNumber, uint32_t Mode);
 void initAsteroids();
-uint16_t updateAsteroids(uint16_t number_of_asteroids, uint16_t counter);
-void drawAsteroids(uint16_t number_of_asteroids);
+uint16_t updateAsteroids(uint16_t counter);
+void drawAsteroids();
 void clearAsteroid(uint16_t x,uint16_t y);
 void clearAsteroids();
+void menu();
 
 
 volatile uint32_t milliseconds;
@@ -42,12 +43,20 @@ const uint16_t rocket[]=
 {
 	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,56053,0,0,0,0,0,0,0,0,0,0,0,56301,56053,0,0,0,0,0,0,0,0,0,6100,56053,63965,13772,0,0,0,0,0,0,0,0,6100,54750,63965,13772,0,0,0,0,0,0,0,0,6100,51603,54750,13772,0,0,0,0,0,0,0,40438,6100,26752,27824,13772,0,0,0,0,0,0,0,20891,12995,59784,4033,12995,20891,0,0,0,0,512,20891,11097,53635,35168,35168,19570,59481,20891,0,0,0,20891,11097,3137,54106,35168,35168,13858,62066,11097,20891,0,0,35946,59712,4106,5130,63763,47122,14363,4106,59712,35946,512,25088,18433,60937,61481,55314,55331,7443,55314,53289,11785,18433,25608,25088,18433,60937,45834,63258,7443,55331,5914,45834,60937,59392,25088,512,34568,19465,53770,54802,5914,5914,5650,53514,19465,34560,8704,0,17664,51457,60937,37130,45834,45834,53514,60937,51457,17408,0,0,8704,1280,51465,19465,11785,60937,19465,51465,1536,8704,0
 };
+const uint16_t explosion[]=
+{
+	0,0,57091,0,0,0,0,0,0,40707,0,0,0,0,40707,40707,14602,31490,0,7683,56835,0,0,0,48899,40707,0,0,6161,22553,0,56593,23555,0,0,40707,48899,40707,55818,47626,30752,38432,5409,31520,31264,0,24323,57091,0,7427,63505,46112,30240,30496,22560,30752,15136,31257,40451,57091,48899,47122,22801,54816,38432,30240,30240,37664,46368,23065,6922,40707,55818,7435,46873,37920,30752,38176,38176,37664,30496,62752,38169,7435,40707,6923,22802,5401,38432,30496,37920,29984,38688,5913,37408,47378,0,0,7427,7698,55840,46880,30496,55840,62744,54809,0,45600,0,0,48899,40707,15890,32281,31264,23065,39186,30994,0,0,0,0,48899,57091,32515,0,7939,63762,5906,55818,57091,0,0,0,48899,32515,0,0,57091,40707,48899,0,0,0,
+};
+
+
+uint16_t number_of_asteroids;
 
 int main()
 {
 	initClock();
 	initSysTick();
 	setupIO();
+	menu();
 	while(1)
 	{
 		clear();
@@ -59,7 +68,7 @@ int main()
 
 		bool game_running = true;
 
-		uint16_t number_of_asteroids = 3;
+		number_of_asteroids = 3;
 		uint16_t counter = 0;
 
 		uint16_t rocket_x = 50;
@@ -74,14 +83,14 @@ int main()
 			hmoved = vmoved = 0;
 			hinverted = vinverted = 0;
 
-			drawAsteroids(number_of_asteroids);
-			counter = updateAsteroids(number_of_asteroids, counter);
+			drawAsteroids();
+			counter = updateAsteroids(counter);
 
 			// This is a potential way to increase amount of asteroids after 3 cycles of asteroids
-			// if ((counter / number_of_asteroids) > 3 && number_of_asteroids < MAX_ASTEROIDS)
-			// {
-			// 	number_of_asteroids++;
-			// }
+			if ((counter / number_of_asteroids) > 3 && number_of_asteroids < MAX_ASTEROIDS)
+			{
+				number_of_asteroids++;
+			}
 
 			if ((GPIOB->IDR & (1 << 4))==0) // right pressed
 			{					
@@ -144,7 +153,8 @@ int main()
 				{
 					// we could use this to display a crash message if the hit an
 					// printTextX2("Crashed!", 10, 20, RGBToWord(0xff,0xff,0), 0);
-					clearAsteroids();
+					clear();
+					putImage(rocket_x,rocket_y,12,12,explosion,0,0);
 					printText("You have crashed!", 5, 5, RGBToWord(0xff,0xff,0), 0);
 					printText("press any button", 5, 25, RGBToWord(0xff,0xff,0), 0);
 					printText("to restart!", 5, 35, RGBToWord(0xff,0xff,0), 0);
@@ -153,7 +163,7 @@ int main()
 					break;
 				}
 			}
-			delay(35);
+			delay(50);
 		}
 		while(game_running == false)
 		{
@@ -258,13 +268,13 @@ void setupIO()
 
 void initAsteroids() {
     for (int i = 0; i < MAX_ASTEROIDS; i++) {
-        asteroids[i].x = (rand() %13) * 10; // Random X position
+        asteroids[i].x = 5 + (rand() %10) * 12; // Random X position
         asteroids[i].y = 0; // Start at the top of the screen
         asteroids[i].speed = 1; 
     }
 }
 
-uint16_t updateAsteroids(uint16_t number_of_asteroids, uint16_t counter) {
+uint16_t updateAsteroids(uint16_t counter) {
     for (int i = 0; i < number_of_asteroids; i++) {
         asteroids[i].y += asteroids[i].speed;
 
@@ -272,7 +282,7 @@ uint16_t updateAsteroids(uint16_t number_of_asteroids, uint16_t counter) {
         if (asteroids[i].y > 150) {
 			counter += 1;
 			fillRectangle(asteroids[i].x, asteroids[i].y - asteroids[i].speed, 10, 10, 0);
-			asteroids[i].x = (rand() %14) * 10;
+			asteroids[i].x = 5 + (rand() %10) * 12;
 			asteroids[i].y = 0;
 			if(asteroids[i].speed < 10)
 			{
@@ -284,7 +294,7 @@ uint16_t updateAsteroids(uint16_t number_of_asteroids, uint16_t counter) {
 }
 
 // Function to draw an asteroid at a specific position
-void drawAsteroids(uint16_t number_of_asteroids) {
+void drawAsteroids() {
 	for(int i = 0; i < number_of_asteroids; i++){
 		clearAsteroid(asteroids[i].x, asteroids[i].y - asteroids[i].speed);
 		putImage(asteroids[i].x, asteroids[i].y, 10, 10, asteroid, 0, 0); // Adjust the size and image as needed
@@ -300,4 +310,12 @@ void clearAsteroids() {
 	for(int i = 0; i < MAX_ASTEROIDS; i++){
 		clearAsteroid(asteroids[i].x, asteroids[i].y - asteroids[i].speed);
 	}
+}
+
+void menu(){
+	printTextX2("Welcome to", 5, 5, RGBToWord(0xff,0xff,0), 0);
+	printTextX2("Space", 30, 30, RGBToWord(0xff,0xff,0), 0);
+	printTextX2("Dodger", 25, 50, RGBToWord(0xff,0xff,0), 0);
+	printText("Dodge the Meteors", 5, 80, RGBToWord(0xff,0xff,0), 0);
+	delay(5000);
 }
